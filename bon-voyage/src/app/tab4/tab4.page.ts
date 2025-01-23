c18191a3736cfa4ba1949691011e7bfce55fdcba
Caitlin Brennan <c00282149@setu.ie>
2025-01-23 12:41:21 +0000

    colours change started

--- bon-voyage/src/app/tab4/tab4.page.ts
+++ bon-voyage/src/app/tab4/tab4.page.ts
@@ -3,7 +3,7 @@ import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/stan
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
 import { IonCard, IonCardContent, IonCardHeader, IonCardSubtitle, IonCardTitle, IonItem, IonLabel, IonList, IonThumbnail, IonButton, IonIcon } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { add } from 'ionicons/icons';
+import { addCircle, arrowForward } from 'ionicons/icons';
 
 @Component({
   selector: 'app-tab4',
@@ -15,6 +15,6 @@ export class Tab4Page {
   public environmentInjector = inject(EnvironmentInjector);
 
   constructor() {
-    addIcons({ add });
+    addIcons({ addCircle, arrowForward });
   }
 }
