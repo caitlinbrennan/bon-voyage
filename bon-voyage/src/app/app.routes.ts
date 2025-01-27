22bb1bd6c182ddd74ced6bfa23be504244da75e2
Caitlin Brennan <c00282149@setu.ie>
2025-01-27 13:08:49 +0000

    pages added for bookmarks / background image changed

--- bon-voyage/src/app/app.routes.ts
+++ bon-voyage/src/app/app.routes.ts
@@ -1,8 +1,33 @@
 import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
 
 export const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./tabs/tabs.routes').then((m) => m.routes),
+  },
+  {
+    path: 'login',
+    loadComponent: () => import('./pages/login/login.page').then( m => m.LoginPage)
+  },
+  {
+    path: 'bookmarks',
+    loadComponent: () => import('./bookmarks/bookmarks.page').then( m => m.BookmarksPage)
+  },
+  {
+    path: 'lists',
+    loadComponent: () => import('./lists/lists.page').then( m => m.ListsPage)
+  },
+  {
+    path: 'bookmarks',
+    loadComponent: () => import('./bookmarks/bookmarks.page').then( m => m.BookmarksPage)
+  },
+  {
+    path: 'locations',
+    loadComponent: () => import('./locations/locations.page').then( m => m.LocationsPage)
+  },
+  {
+    path: 'bookmarks',
+    loadComponent: () => import('./bookmarks/bookmarks.page').then( m => m.BookmarksPage)
   },
 ];
--- bon-voyage/src/environments/environment.ts
+++ bon-voyage/src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  supabaseUrl: 'https://aoesmwcahajxuceweegh.supabase.co',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFvZXNtd2NhaGFqeHVjZXdlZWdoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mzc0NTUzNTIsImV4cCI6MjA1MzAzMTM1Mn0.5VS7oPNphGRA-naaxCcUSvocQ_5ozh4-R68m4Bxswqw'
 };
 
 /*
